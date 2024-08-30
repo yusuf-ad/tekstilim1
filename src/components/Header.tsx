e6ed1a40969cef085824db81ea5753c7ab6d2dbd
yusuf <yusufgad24@gmail.com>
2024-08-30 09:39:37 +0300

    Add navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,16 @@
 import Button from "./Button";
-import MegaMenu from "./MegaMenu";
-
-interface Navbarprops {
-  id: number;
-  title: string;
-  link: string;
-  icon?: string;
-  submenu?: true;
-}
-
-const Navbardata: Navbarprops[] = [
-  { id: 1, title: "Ana Sayfa", link: "#" },
-  {
-    id: 2,
-    title: "Mağaza",
-    icon: "fa-solid fa-chevron-down",
-    link: "#",
-    submenu: true,
-  },
-  { id: 3, title: "Hikaye", link: "#" },
-  { id: 4, title: "Blog", link: "#" },
-  { id: 5, title: "Bize Ulaşın", link: "#" },
-];
+import Navbar from "./Navbar";
 
 function Header() {
   return (
     <header className="container mx-auto flex max-w-screen-2xl items-center justify-between px-8 py-6">
-      {/* logo */}
       <div>
-        <h1 className="text-4xl font-semibold">tekstilim</h1>
+        <a href="#">
+          <h1 className="text-4xl font-semibold">tekstilim</h1>
+        </a>
       </div>
-      {/* navbar */}
-      <nav>
-        <ul className="hidden items-center gap-4 lg:flex">
-          {Navbardata.map((data) => (
-            <li className="group relative p-2 py-4" key={data.id}>
-              <a
-                className="flex items-center text-lg font-semibold text-primary-400 hover:text-primary-900"
-                href={data.link}
-                // onClick={
-                //   data.title.includes("Mağaza")
-                //     ? handleMegaMenuToggle
-                //     : undefined
-                // }
-              >
-                {data.title}
-                {data.icon && (
-                  <i className={`fa ml-[6px] text-sm ${data.icon}`}></i>
-                )}
-              </a>
-              {data.submenu && <MegaMenu />}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      {/* button */}
+
+      <Navbar />
 
       <div className="flex items-center gap-8">
         <div className="hidden gap-8 lg:flex">
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import MegaMenu from "./MegaMenu";
+
+interface Navbarprops {
+  id: number;
+  title: string;
+  link: string;
+  icon?: string;
+  submenu?: true;
+}
+
+const Navbardata: Navbarprops[] = [
+  { id: 1, title: "Ana Sayfa", link: "#" },
+  {
+    id: 2,
+    title: "Mağaza",
+    icon: "fa-solid fa-chevron-down",
+    link: "#",
+    submenu: true,
+  },
+  { id: 3, title: "Hikaye", link: "#" },
+  { id: 4, title: "Blog", link: "#" },
+  { id: 5, title: "Bize Ulaşın", link: "#" },
+];
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="hidden items-center gap-4 lg:flex">
+        {Navbardata.map((data) => (
+          <li className="group relative p-2 py-4" key={data.id}>
+            <a
+              className="flex items-center text-lg font-semibold text-primary-400 hover:text-primary-900"
+              href={data.link}
+            >
+              {data.title}
+              {data.icon && (
+                <i className={`fa ml-[6px] text-sm ${data.icon}`}></i>
+              )}
+            </a>
+            {data.submenu && <MegaMenu />}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
